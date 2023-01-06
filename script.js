044313a02f4e05625ff7295a58bc5b3305de78c3
Muhammed Yasir Özdemir <yasiryksmail@gmail.com>
2023-01-06 23:34:07 +0300

    files create
    structure almost done

---
+++ script.js
@@ -0,0 +1,193 @@
+const questionsEasy = [
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "What does CPU stand for?",
+    correct_answer: "Central Processing Unit",
+    incorrect_answers: [
+      "Central Process Unit",
+      "Computer Personal Unit",
+      "Central Processor Unit",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn't get modified?",
+    correct_answer: "Final",
+    incorrect_answers: ["Static", "Private", "Public"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "The logo for Snapchat is a Bell.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question:
+      "Pointers were not used in the original C programming language; they were added later on in C++.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "What is the most preferred image format used for logos in the Wikimedia database?",
+    correct_answer: ".svg",
+    incorrect_answers: [".png", ".jpeg", ".gif"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "In web design, what does CSS stand for?",
+    correct_answer: "Cascading Style Sheet",
+    incorrect_answers: [
+      "Counter Strike: Source",
+      "Corrective Style Sheet",
+      "Computer Style Sheet",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "What is the code name for the mobile operating system Android 7.0?",
+    correct_answer: "Nougat",
+    incorrect_answers: ["Ice Cream Sandwich", "Jelly Bean", "Marshmallow"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question: "On Twitter, what is the character limit for a Tweet?",
+    correct_answer: "140",
+    incorrect_answers: ["120", "160", "100"],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "easy",
+    question: "Linux was first created as an alternative to Windows XP.",
+    correct_answer: "False",
+    incorrect_answers: ["True"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "easy",
+    question:
+      "Which programming language shares its name with an island in Indonesia?",
+    correct_answer: "Java",
+    incorrect_answers: ["Python", "C", "Jakarta"],
+  },
+];
+
+const questionsDifficult = [
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "difficult",
+    question:
+      "According to the International System of Units, how many bytes are in a kilobyte of RAM?",
+    correct_answer: "1000",
+    incorrect_answers: ["512", "1024", "500"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "difficult",
+    question:
+      "What is the most preferred image format used for logos in the Wikimedia database?",
+    correct_answer: ".svg",
+    incorrect_answers: [".png", ".jpeg", ".gif"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "difficult",
+    question: "In web design, what does CSS stand for?",
+    correct_answer: "Cascading Style Sheet",
+    incorrect_answers: [
+      "Counter Strike: Source",
+      "Corrective Style Sheet",
+      "Computer Style Sheet",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "difficult",
+    question: "RAM stands for Random Access Memory.",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "difficult",
+    question:
+      "The numbering system with a radix of 16 is more commonly referred to as ",
+    correct_answer: "Hexidecimal",
+    incorrect_answers: ["Binary", "Duodecimal", "Octal"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "difficult",
+    question: "How many values can a single byte represent?",
+    correct_answer: "256",
+    incorrect_answers: ["8", "1", "1024"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "difficult",
+    question:
+      "The C programming language was created by this American computer scientist. ",
+    correct_answer: "Dennis Ritchie",
+    incorrect_answers: ["Tim Berners Lee", "al-Khwarizmi", "Willis Ware"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "difficult",
+    question: "In computing, what does MIDI stand for?",
+    correct_answer: "Musical Instrument Digital Interface",
+    incorrect_answers: [
+      "Musical Interface of Digital Instruments",
+      "Modular Interface of Digital Instruments",
+      "Musical Instrument Data Interface",
+    ],
+  },
+  {
+    category: "Science: Computers",
+    type: "boolean",
+    difficulty: "difficult",
+    question:
+      "The Python programming language gets its name from the British comedy group &quot;Monty Python.&quot;",
+    correct_answer: "True",
+    incorrect_answers: ["False"],
+  },
+  {
+    category: "Science: Computers",
+    type: "multiple",
+    difficulty: "difficult",
+    question: "What language does Node.js use?",
+    correct_answer: "JavaScript",
+    incorrect_answers: ["Java", "Java Source", "Joomla Source Code"],
+  },
+];
+
+window.onload = function () {};
